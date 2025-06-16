7af92b887731e5070fecc109204e4fd532fd3cac
zen_vega <ufogreeks@gmail.com>
2025-06-16 15:40:32 +0200

    feat: log action function

--- ex01/utils/utils.cpp
+++ ex01/utils/utils.cpp
@@ -6,35 +6,51 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 14:56:13 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/06/16 14:58:06 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/06/16 15:35:23 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
-static std::array<std::string, NUM_PROMPTS> init_prompts() {
-  std::array<std::string, NUM_PROMPTS> prompts;
+static std::string get_prompt(prompt_code prompt) {
 
-  prompts[FIRST_NAME] = "enter first name";
-  prompts[LAST_NAME] = "enter second name";
-  prompts[NICKNAME] = "enter nickname";
-  prompts[PHONE_NUMBER] = "enter phone number";
-  prompts[SECRET] = "tell me your deepest secret";
-  prompts[COMMAND] = "enter ADD, SEARCH or EXIT";
+  if (prompt == FIRST_NAME)
+    return ("enter first name");
+  else if (prompt == LAST_NAME)
+    return ("enter second name");
+  else if (prompt == NICKNAME)
+    return ("enter nickname");
+  else if (prompt == PHONE_NUMBER)
+    return ("enter phone number");
+  else if (prompt == SECRET)
+    return ("tell me your deepest secret");
+  else if (prompt == COMMAND)
+    return ("enter ADD, SEARCH or EXIT");
 
-  return (prompts);
+  return ("error");
+}
+
+static std::string get_action(action_code prompt) {
+
+  if (prompt == EMPTY)
+    return (": field cannot be empty");
+  else if (prompt == NO_NUMBER)
+    return (": has to be only numbers");
+
+  return ("error");
 }
 
 std::string get_user_input(prompt_code prompt) {
 
-  std::array<std::string, NUM_PROMPTS> prompts;
-
-  prompts = init_prompts();
   std::string input;
-  std::cout << prompts[prompt] << std::endl;
+  std::cout << get_prompt(prompt) << std::endl;
   std::getline(std::cin, input);
   return input;
 }
 
-static void display_entries(void) {};
-int log_action(std::string subject, action_code action) {}
+void display_entries(void) {};
+
+int log_action(std::string subject, action_code action) {
+  std::cout << subject << get_action(action) << std::endl;
+  return (0);
+}
--- ex01/utils/utils.hpp
+++ ex01/utils/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 14:56:18 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/06/16 15:01:30 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/06/16 15:33:14 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define UTILS_HPP
 
 #include "../includes/CONSTANTS.h"
+#include <iostream>
 #include <string>
 
 int log_action(std::string subject, action_code action);
